1675aa1a7cd5445e48dacbb29d501a37d2fe84d8
tiagojleandro <leandrotiago223@gmail.com>
2021-07-20 22:07:43 -0300

    finalizando o exercicio 1041 com python

---
+++ python/uri_1041.py
@@ -0,0 +1,32 @@
+"""
+Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+Entrada
+
+A entrada contem as coordenadas de um ponto.
+Saída
+
+A saída deve apresentar o quadrante em que o ponto se encontra.
+
+"""
+
+def encontrar_posicao_no_plano_cartesiano():
+    eixo_x, eixo_y = [float(eixo) for eixo in input().split()]
+    if eixo_x == 0.0 and eixo_y == 0.0:
+        print("Origem")
+        return
+    elif eixo_x == 0.0:
+        print("Eixo Y")
+        return
+    elif eixo_y == 0.0:
+        print("Eixo X")
+        return
+    quadrantes = ['Q1', 'Q4'] if eixo_x >= 0 else ['Q2', 'Q3']
+    index = 0 if eixo_y >= 0 else 1
+    print(quadrantes[index])
+    return
+
+encontrar_posicao_no_plano_cartesiano()
