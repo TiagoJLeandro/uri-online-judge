3b3c46591efc8945b896cda3084152981051dd8d
TiagoJLeandro <leandrotiago223@gmail.com>
2021-07-22 13:52:05 -0300

    finalizando o exercicio 1049 com python

---
+++ python/uri_1049.py
@@ -0,0 +1,33 @@
+"""
+Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+
+"""
+
+animal = {
+    "vertebrado": {
+        "ave": {
+            "carnivoro": "aguia",
+            "onivoro": "pomba"
+        },
+        "mamifero": {
+            "onivoro": "homem",
+            "herbivoro": "vaca"
+        }
+    },
+    "invertebrado": {
+        "inseto": {
+            "hematofago": "pulga",
+            "herbivoro": "lagarta"
+        },
+        "anelideo": {
+            "hematofago": "sanguessuga",
+            "onivoro": "minhoca"
+        }
+    }
+}
+
+subfilo = input()
+classe = input()
+alimentacao = input()
+
+print(f"{animal[subfilo][classe][alimentacao]}")
