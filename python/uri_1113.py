0260182cd9640aba3481d28601d838c2202e4b65
tiagojleandro <leandrotiago223@gmail.com>
2021-08-06 07:15:04 -0300

    finalizado o exercicio 1113 com python

---
+++ python/uri_1113.py
@@ -0,0 +1,18 @@
+"""
+Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente.
+Entrada
+
+A entrada contém vários casos de teste. Cada caso contém dois valores inteiros X e Y. A leitura deve ser encerrada ao ser fornecido valores iguais para X e Y.
+Saída
+
+Para cada caso de teste imprima “Crescente”, caso os valores tenham sido digitados na ordem crescente, caso contrário imprima a mensagem “Decrescente”.
+"""
+
+x, y = 0, 1
+
+while x != y:
+    x, y = [int(num) for num in input().split()]
+    if x == y:
+        break
+    mensagem = "Crescente" if x < y else "Decrescente"
+    print(mensagem)
